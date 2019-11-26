88deb3a8f05d4e09f503b2a7fcae076cd8f404e8
silverskg <chokichoki_gm@i.softbank.jp>
2019-11-26 09:52:17 +0900

    chat-space

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,11 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    confing.generators do |g|
+    config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
+    end
   end
 end

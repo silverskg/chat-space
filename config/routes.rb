4dde2192d64bf461025ce1e2295ab1b32bfe170e
silverskg <chokichoki_gm@i.softbank.jp>
2019-11-27 19:36:14 +0900

    chatcpace

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: "messages#index"
   devise_for :users
   resource :users, only: [:index, :edit, :update]
-  resource :groups, only: [:new, :create, :edit, :update]
+  # resource :groups, only: [:new, :create, :edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191127031232) do
+ActiveRecord::Schema.define(version: 20191127072704) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
@@ -29,8 +29,6 @@ ActiveRecord::Schema.define(version: 20191127031232) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["name"], name: "index_users_on_name", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
